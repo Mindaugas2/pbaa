cf350976661015054c713679da1e1b207bdea80a
Ernestas <ernestas.urbonas57@gmail.com>
2022-05-26 14:55:53 +0300

    make CSV button background color look same as edit and delete button

--- frontend/src/components/Expense.js
+++ frontend/src/components/Expense.js
@@ -652,7 +652,7 @@ export default function Expense() {
 
           </div>
           {/* This section is CSV export button */}
-          <button type="submit" className="btn btn-lg" style={{ float: "right", backgroundColor: "#74bdee" }}>
+          <button type="submit" className="btn btn-lg" style={{ float: "right", backgroundColor: "#008F8C" }}>
             <ReactCSV allExpenses={allExpense2} />
           </button>
 
